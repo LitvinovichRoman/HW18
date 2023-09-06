349e9cb2d8672d23d821eb6e7549cc7b7be4a3d9
yeerryy <127106050+yeerryy@users.noreply.github.com>
2023-09-06 11:23:15 +0300

    Project created

---
+++ HW17/HW17/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW17
+//
+//  Created by Роман Литвинович on 6.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
